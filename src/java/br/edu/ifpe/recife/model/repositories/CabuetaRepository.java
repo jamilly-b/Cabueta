8aba661e649c3f335577a9205e4585ae28044ffb
jamilly-b <jbm2@discente.ifpe.edu.br>
2024-03-22 16:08:52 -0300

    Criação das classes, repositório e controlador.

---
+++ src/java/br/edu/ifpe/recife/model/repositories/CabuetaRepository.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpe.recife.model.repositories;
+
+/**
+ *
+ * @author jamilly
+ */
+public class CabuetaRepository {
+    
+    public static void creat(){
+        
+    }
+    public static void read(){
+        
+    }
+    public static void update(){
+        
+    }
+    public static void delete(){
+        
+    }
+}
